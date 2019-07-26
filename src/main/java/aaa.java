3ae0ddde8a7de8e53b794ac04943eebd7bbb90dc
Lif20000209 <puhaha0209>
2019-07-26 18:57:32 +0800

    aaa测试2

---
+++ src/main/java/aaa.java
@@ -0,0 +1,7 @@
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+        System.out.println("bbbbb");
+        System.out.println("vcccc");
+    }
+}
