5326600174e5d37de0a7310cdac2744460d7ac01
Lif20000209 <puhaha0209>
2019-07-29 08:28:01 +0800

    aaa测试2

--- src/main/java/aaa.java
+++ src/main/java/aaa.java
@@ -1,11 +1,8 @@
 public class aaa {
     public static void main(String[] args) {
-        System.out.println("aaaaa");
-        System.out.println("bbbbb");
-        System.out.println("vcccc");
-        System.out.println("vcccc");
-        System.out.println("vcccc");
-        System.out.println("vcccc");
-        System.out.println("vcccc");
+        System.out.println("哈哈哈哈哈");
+        System.out.println("呵呵呵呵呵");
+        System.out.println("呵呵呵呵呵");
+        System.out.println("哈哈哈哈哈");
     }
 }
